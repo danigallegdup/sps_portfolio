491899eb97729faff6097efafc43c75b9d9f064a
Daniela <danigallegdup@gmail.com>
2022-07-05 11:47:33 -0700

    reorganized content by week

--- portfolio/src/main/webapp/scripts/script.js
+++ portfolio/src/main/webapp/scripts/script.js
@@ -8,6 +8,21 @@
     header.classList.add("sticky");
 
   }
+/**
+ * Adds a random greeting to the page.
+ */
+ function addRandomGreeting() {
+  const greetings =
+      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+
+  // Pick a random greeting.
+  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+  // Add it to the page.
+  const greetingContainer = document.getElementById('greeting-container');
+  greetingContainer.innerText = greeting;
+}
+
 
   /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
