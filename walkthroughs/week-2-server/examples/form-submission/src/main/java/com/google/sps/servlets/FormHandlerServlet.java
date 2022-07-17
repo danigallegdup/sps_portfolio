926db2b516018b0523772a64f4bd9f422e9b3d26
Daniela Gallegos Dupuis <danigallegdup@gmail.com>
2022-07-17 19:43:55 +0000

    Finished basic Form

--- walkthroughs/week-2-server/examples/form-submission/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ walkthroughs/week-2-server/examples/form-submission/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -26,18 +26,21 @@ public class FormHandlerServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String name = request.getParameter("name-input");
-    String color = request.getParameter("color-input");
+    String email = request.getParameter("email-input");
+    String role = request.getParameter("role-input");
     String description = request.getParameter("description-input");
 
     // // Print the input so you can see it in the server logs.
     System.out.println("name: " + name);
-    System.out.println("color: " + color);
+    System.out.println("email " + name);
+    System.out.println("role: " + role);
     System.out.println("description: " + description);
 
     // Write the input to the response so the user can see it.
     response.setContentType("text/html;");
     response.getWriter().println("<p>Name: " + name + "</p>");
-    response.getWriter().println("<p>Color: " + color + "</p>");
+    response.getWriter().println("<p>Name: " + email + "</p>");
+    response.getWriter().println("<p>Role: " + role + "</p>");
     response.getWriter().println("<p>Description: " + description + "</p>");
   }
 }
